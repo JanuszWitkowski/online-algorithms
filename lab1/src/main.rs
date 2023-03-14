040307d1c8aef3806e83732d059b9990fbad4f4e
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-03-14 11:49:52 +0100

    Implement Counting UpdateList

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -7,9 +7,15 @@ fn test_trait (ul: &mut dyn update_list::UpdateList) {
 
 fn main() {
     // let mut ul = update_list::ULClassic::new();
-    let mut ul = update_list::ULMoveToFront::new();
+    // let mut ul = update_list::ULMoveToFront::new();
+    // let mut ul = update_list::ULTranspose::new();
+    let mut ul = update_list::ULCount::new();
     test_trait(&mut ul);
     ul.access(1_u8);
+    test_trait(&mut ul);
+    ul.access(2_u8);
+    test_trait(&mut ul);
+    ul.access(2_u8);
     test_trait(&mut ul);
     ul.access(2_u8);
     test_trait(&mut ul);
--- lab1/src/update_list.rs
+++ lab1/src/update_list.rs
@@ -1,74 +1,120 @@
-// mod update_list {
-    pub trait UpdateList {
-        fn new() -> Self where Self: Sized;
-        fn access(&mut self, x: u8) -> u8;
-        fn print(&mut self);
-    }
-    
-    fn access_search(list: &mut Vec<u8>, x: u8) -> (u8, usize) {
-        let mut cost: u8 = 0;
-        let mut i = 0;
-        let n = list.len();
-        while i < n && list[i] != x {
-            cost += 1;
-            i += 1;
-        }
-        if i == n {
-            list.insert(n, x);
-        } else {
-            cost += 1;
-        }
-        return (cost, i);   // (cost, index of the element found/inserted)
-    }
+pub trait UpdateList {
+    fn new() -> Self where Self: Sized;
+    fn access(&mut self, x: u8) -> u8;
+    fn print(&mut self);
+}
 
-    fn print_list(list: &Vec<u8>) {
+fn access_search(list: &mut Vec<u8>, x: u8) -> (u8, usize, bool) {
+    let mut cost: u8 = 0;
+    let mut i = 0;
+    let inserted: bool;
+    let n = list.len();
+    while i < n && list[i] != x {
+        cost += 1;
+        i += 1;
+    }
+    if i == n {
+        list.insert(n, x);
+        inserted = true;
+    } else {
+        inserted = false;
+        cost += 1;
+    }
+    return (cost, i, inserted);   // (cost, index of the element found/inserted)
+}
+
+fn print_list(list: &Vec<u8>) {
+    print!("[");
+    for el in list {
+        print!(" {}", el);
+    }
+    println!(" ]");
+}
+
+pub struct ULClassic {
+    list: Vec<u8>,
+}
+impl UpdateList for ULClassic {
+    fn new() -> Self {
+        return ULClassic{list: Vec::new()};
+    }
+    fn access(&mut self, x: u8) -> u8 {
+        let (cost, _, _) = access_search(&mut self.list, x);
+        return cost;
+    }
+    fn print(&mut self) {
+        print_list(&self.list);
+    }
+}
+
+pub struct ULMoveToFront {
+    list: Vec<u8>,
+}
+impl UpdateList for ULMoveToFront {
+    fn new() -> Self {
+        return ULMoveToFront { list: Vec::new() }
+    }
+    fn access(&mut self, x: u8) -> u8 {
+        let (cost, index, _) = access_search(&mut self.list, x);
+        self.list.remove(index);
+        self.list.insert(0, x);
+        return cost;
+    }
+    fn print(&mut self) {
+        print_list(&self.list);
+    }
+}
+
+pub struct ULTranspose {
+    list: Vec<u8>,
+}
+impl UpdateList for ULTranspose {
+    fn new() -> Self {
+        return ULTranspose { list: Vec::new() }
+    }
+    fn access(&mut self, x: u8) -> u8 {
+        let (cost, index, _) = access_search(&mut self.list, x);
+        if index > 0 {
+            self.list.remove(index);
+            self.list.insert(index - 1, x);
+        }
+        return cost;
+    }
+    fn print(&mut self) {
+        print_list(&self.list);
+    }
+}
+
+pub struct ULCount {
+    list: Vec<u8>,
+    freq: Vec<u32>,
+}
+impl UpdateList for ULCount {
+    fn new() -> Self {
+        return ULCount { list: Vec::new(), freq: Vec::new() }
+    }
+    fn access(&mut self, x: u8) -> u8 {
+        let (cost, index, inserted) = access_search(&mut self.list, x);
+        if inserted {
+            self.freq.insert(index, 0);
+        }
+        let mut i: i8 = (index as i8) - 1;
+        let frequency = self.freq[index] + 1;
+        self.list.remove(index);
+        self.freq.remove(index);
+        while i >= 0 && self.freq[i as usize] <= frequency {
+            i -= 1;
+        }
+        self.list.insert((i + 1) as usize, x);
+        self.freq.insert((i + 1) as usize, frequency);
+        return cost;
+    }
+    fn print(&mut self) {
         print!("[");
-        for el in list {
-            print!(" {}", el);
+        for i in 0..self.list.len() {
+            print!(" {}({})", self.list[i], self.freq[i]);
         }
         println!(" ]");
     }
-    
-    pub struct ULClassic {
-        list: Vec<u8>,
-    }
-    impl UpdateList for ULClassic {
-        fn new() -> Self {
-            return ULClassic{list: Vec::new()};
-        }
-        fn access(&mut self, x: u8) -> u8 {
-            let (cost, _) = access_search(&mut self.list, x);
-            return cost;
-        }
-        fn print(&mut self) {
-            print_list(&self.list);
-        }
-    }
-    
-    pub struct ULMoveToFront {
-        list: Vec<u8>,
-    }
-    impl UpdateList for ULMoveToFront {
-        fn new() -> Self {
-            return ULMoveToFront { list: Vec::new() }
-        }
-        fn access(&mut self, x: u8) -> u8 {
-            let (cost, index) = access_search(&mut self.list, x);
-            self.list.remove(index);
-            self.list.insert(0, x);
-            return cost;
-        }
-        fn print(&mut self) {
-            print_list(&self.list);
-        }
-    }
-    
-    pub struct ULTranspose {
-        list: Vec<u8>,
-    }
-    
-    pub struct ULCount {
-        list: Vec<(u8, u32)>,
-    }
-    
-// }
+}
+
