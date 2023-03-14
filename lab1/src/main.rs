0bb2bfb0513f24589410c7c18aa50ee5da09e5b2
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-03-14 12:40:13 +0100

    Test distribution and trait forwarding

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -3,31 +3,21 @@ use update_list::UpdateList;
 mod distribution;
 use distribution::Distribution;
 
-fn test_trait (ul: &mut dyn update_list::UpdateList) {
+const _NS: [u32; 7] = [100, 500, 1000, 5000, 10000, 50000, 100000];
+
+fn test_trait (ul: &mut dyn update_list::UpdateList, ds: &dyn distribution::Distribution) {
+    ul.access(ds.get());
     ul.print();
 }
 
 fn main() {
-    let ds = distribution::Uniform::new(1, 100);
-    print!("Random: ");
-    for _ in 0..10 {
-        print!("{} ", ds.get());
-    }
-    println!();
     // let mut ul = update_list::ULClassic::new();
     // let mut ul = update_list::ULMoveToFront::new();
     // let mut ul = update_list::ULTranspose::new();
     let mut ul = update_list::ULCount::new();
-    test_trait(&mut ul);
-    ul.access(1_u8);
-    test_trait(&mut ul);
-    ul.access(2_u8);
-    test_trait(&mut ul);
-    ul.access(2_u8);
-    test_trait(&mut ul);
-    ul.access(2_u8);
-    test_trait(&mut ul);
-    let access_cost: u8 = ul.access(3_u8);
-    println!("Hello, world! {}", access_cost);
-    test_trait(&mut ul);
+    let ds = distribution::Uniform::new(1, 100);
+    ul.print();
+    for _ in 0..20 {
+        test_trait(&mut ul, &ds);
+    }
 }
