c83d3bb4d833c4f006c8ebf96672cb94c53ed860
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-03-14 12:47:58 +0100

    Add LOW and HIGH constants in main

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -4,6 +4,8 @@ mod distribution;
 use distribution::Distribution;
 
 const _NS: [u32; 7] = [100, 500, 1000, 5000, 10000, 50000, 100000];
+const _LOW: u8 = 1;
+const _HIGH: u8 = 100;
 
 fn test_trait (ul: &mut dyn update_list::UpdateList, ds: &dyn distribution::Distribution) {
     ul.access(ds.get());
@@ -15,7 +17,7 @@ fn main() {
     // let mut ul = update_list::ULMoveToFront::new();
     // let mut ul = update_list::ULTranspose::new();
     let mut ul = update_list::ULCount::new();
-    let ds = distribution::Uniform::new(1, 100);
+    let ds = distribution::Uniform::new(1, 10);
     ul.print();
     for _ in 0..20 {
         test_trait(&mut ul, &ds);
