f7ad74337ed747b15dadc8cfa83ce1bc57222b53
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2023-03-30 19:40:48 +0200

    Calculate expected value in Lab1

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -28,17 +28,16 @@ fn experiment_average_access_cost (ul: &mut dyn update_list::UpdateList,
     return sum / ((n as f64) * (iter as f64));
 }
 
-
-fn main() -> std::io::Result<()> {
+fn run_experiments (ns: &[usize], iter: usize, limit: u8) {
     // Set up.
     let mut ul_classic = update_list::ULClassic::new();
     let mut ul_move_to_front = update_list::ULMoveToFront::new();
     let mut ul_transpose = update_list::ULTranspose::new();
     let mut ul_count = update_list::ULCount::new();
-    let ds_uniform = distribution::Uniform::new(LIMIT);
-    let ds_geometric = distribution::Geometric::new(LIMIT);
-    let ds_harmonic = distribution::Harmonic::new(LIMIT);
-    let ds_two_harmonic = distribution::TwoHarmonic::new(LIMIT);
+    let ds_uniform = distribution::Uniform::new(limit);
+    let ds_geometric = distribution::Geometric::new(limit);
+    let ds_harmonic = distribution::Harmonic::new(limit);
+    let ds_two_harmonic = distribution::TwoHarmonic::new(limit);
     let update_lists: [&mut dyn UpdateList; 4] = [&mut ul_classic, &mut ul_move_to_front, &mut ul_transpose, &mut ul_count];
     let distributions: [&dyn Distribution; 4] = [&ds_uniform, &ds_geometric, &ds_harmonic, &ds_two_harmonic];
     let mut cost: f64;
@@ -54,7 +53,7 @@ fn main() -> std::io::Result<()> {
         Ok(file) => file,
         Err(error) => panic!("Problem creating ns file : {:?}", error),
     };
-    for n in NS {
+    for n in ns {
         write!(output, "{}\n", n).expect("Error writing NS file.");
     }
 
@@ -68,12 +67,42 @@ fn main() -> std::io::Result<()> {
                 Ok(file) => file,
                 Err(error) => panic!("Problem creating data file : {:?}", error),
             };
-            for n in NS {
-                cost = experiment_average_access_cost(ul, ds, n, ITER);
+            for n in ns {
+                cost = experiment_average_access_cost(ul, ds, *n, iter);
                 write!(output, "{}\n", cost).expect("Error writing in data file.");
             }
         }
     }
     println!("Done!");
+}
+
+
+fn expected_value (ds: &dyn distribution::Distribution, iter: usize) -> f64 {
+    let mut sum: f64 = 0.0;
+    for _ in 0..iter {
+        sum += ds.get() as f64;
+    }
+    return sum / (iter as f64);
+}
+
+fn run_ev (iter: usize, limit: u8) {
+    let ds_uniform = distribution::Uniform::new(limit);
+    let ds_geometric = distribution::Geometric::new(limit);
+    let ds_harmonic = distribution::Harmonic::new(limit);
+    let ds_two_harmonic = distribution::TwoHarmonic::new(limit);
+    let ev_uniform = expected_value(&ds_uniform, iter);
+    let ev_geometric = expected_value(&ds_geometric, iter);
+    let ev_harmonic = expected_value(&ds_harmonic, iter);
+    let ev_twoharmonic = expected_value(&ds_two_harmonic, iter);
+    println!("uni: {}", ev_uniform);
+    println!("geo: {}", ev_geometric);
+    println!("harm: {}", ev_harmonic);
+    println!("2harm: {}", ev_twoharmonic);
+}
+
+
+fn main() -> std::io::Result<()> {
+    run_ev(100_000, LIMIT);
+    run_experiments(&NS, ITER, LIMIT);
     Ok(())
 }
