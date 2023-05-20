7b8e35adfe2f8ad41148a9502acd2cc687297722
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2023-05-20 12:11:55 +0200

    results p1

--- lab3/src/main.rs
+++ lab3/src/main.rs
@@ -17,17 +17,17 @@ use crate::fits::worst::*;
 
 const SEQUENCE_LIMIT    : usize = 100;
 const DIST_LIMIT        : usize = 10;
-const N_OF_EXPERIMENTS  : usize = 1_000_000;
+const N_OF_EXPERIMENTS  : usize = 10_000;
 // const N_OF_EXPERIMENTS  : usize = 10;
-const RANDOMFIT_ITERS   : usize = 10_000;
+const RANDOMFIT_ITERS   : usize = 100;
 
 fn main() {
-    let fits: [&mut dyn Fit; 4] = [
-        &mut NextFit::new(),
-        &mut FirstFit::new(),
-        &mut BestFit::new(),
-        &mut WorstFit::new()
-    ];
+    // let fits: [&mut dyn Fit; 4] = [
+    //     &mut NextFit::new(),
+    //     &mut FirstFit::new(),
+    //     &mut BestFit::new(),
+    //     &mut WorstFit::new()
+    // ];
     let mut fit_random = RandomFit::new();
     let distributions: [&dyn Distribution; 4] = [
         &Uniform::new(DIST_LIMIT),
@@ -35,11 +35,11 @@ fn main() {
         &Harmonic::new(DIST_LIMIT),
         &Diharmonic::new(DIST_LIMIT)
     ];
-    for fit in fits {
-        for dist in distributions {
-            experiment::run_bit_packings(fit, dist, SEQUENCE_LIMIT, N_OF_EXPERIMENTS);
-        }
-    }
+    // for fit in fits {
+    //     for dist in distributions {
+    //         experiment::run_bit_packings(fit, dist, SEQUENCE_LIMIT, N_OF_EXPERIMENTS);
+    //     }
+    // }
     for dist in distributions {
         experiment::multiple_bit_packings(&mut fit_random, dist, SEQUENCE_LIMIT, N_OF_EXPERIMENTS, RANDOMFIT_ITERS);
     }
